9c1a5d9348f789b31347e51e265504a1e71d4954
Fernanda Costa <fernandacosta@alu.ufc.br>
2022-05-12 19:28:35 -0300

    feat:(project)

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://mongodb:password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+}
